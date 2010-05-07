ec340ac5c6912d75c183958e9353da619d632a99
Jens Wille <jens.wille@uni-koeln.de>
2010-05-07 12:24:07 +0200

    lib/wadl.rb: License header.

--- lib/wadl.rb
+++ lib/wadl.rb
@@ -1,8 +1,30 @@
-# wadl.rb (http://www.crummy.com/software/wadl.rb/)
-# Super cheap Ruby WADL client
-# by Leonard Richardson leonardr@segfault.org
-# v20070217
-# For more on WADL, see http://wadl.dev.java.net/
+#--
+###############################################################################
+#                                                                             #
+# wadl -- Super cheap Ruby WADL client                                        #
+#                                                                             #
+# Copyright (C) 2006-2008 Leonard Richardson                                  #
+# Copyright (C) 2010 Jens Wille                                               #
+#                                                                             #
+# Authors:                                                                    #
+#     Leonard Richardson <leonardr@segfault.org> (Original author)            #
+#     Jens Wille <jens.wille@uni-koeln.de>                                    #
+#                                                                             #
+# wadl is free software; you can redistribute it and/or modify it under the   #
+# terms of the GNU General Public License as published by the Free Software   #
+# Foundation; either version 3 of the License, or (at your option) any later  #
+# version.                                                                    #
+#                                                                             #
+# wadl is distributed in the hope that it will be useful, but WITHOUT ANY     #
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS   #
+# FOR A PARTICULAR PURPOSE. See the GNU General Public License for more       #
+# details.                                                                    #
+#                                                                             #
+# You should have received a copy of the GNU General Public License along     #
+# with wadl. If not, see <http://www.gnu.org/licenses/>.                      #
+#                                                                             #
+###############################################################################
+#++
 
 %w[delegate rexml/document set cgi yaml rubygems rest-open-uri].each { |lib|
   require lib
