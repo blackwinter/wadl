4faa5c6ca4de42b9e3f998d8dbbc739b1ff196a4
Jens Wille <jens.wille@uni-koeln.de>
2011-04-29 15:08:38 +0200

    Switched license to Affero GPL.

--- lib/wadl.rb
+++ lib/wadl.rb
@@ -4,24 +4,24 @@
 # wadl -- Super cheap Ruby WADL client                                        #
 #                                                                             #
 # Copyright (C) 2006-2008 Leonard Richardson                                  #
-# Copyright (C) 2010 Jens Wille                                               #
+# Copyright (C) 2010-2011 Jens Wille                                          #
 #                                                                             #
 # Authors:                                                                    #
 #     Leonard Richardson <leonardr@segfault.org> (Original author)            #
 #     Jens Wille <jens.wille@uni-koeln.de>                                    #
 #                                                                             #
 # wadl is free software; you can redistribute it and/or modify it under the   #
-# terms of the GNU General Public License as published by the Free Software   #
-# Foundation; either version 3 of the License, or (at your option) any later  #
-# version.                                                                    #
+# terms of the GNU Affero General Public License as published by the Free     #
+# Software Foundation; either version 3 of the License, or (at your option)   #
+# any later version.                                                          #
 #                                                                             #
 # wadl is distributed in the hope that it will be useful, but WITHOUT ANY     #
 # WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS   #
-# FOR A PARTICULAR PURPOSE. See the GNU General Public License for more       #
-# details.                                                                    #
+# FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for     #
+# more details.                                                               #
 #                                                                             #
-# You should have received a copy of the GNU General Public License along     #
-# with wadl. If not, see <http://www.gnu.org/licenses/>.                      #
+# You should have received a copy of the GNU Affero General Public License    #
+# along with wadl. If not, see <http://www.gnu.org/licenses/>.                #
 #                                                                             #
 ###############################################################################
 #++
